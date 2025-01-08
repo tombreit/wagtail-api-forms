ce8243f900c74675b2a03219b9efb0405b47af78
Thomas Breitner <mail@thms.de>
2025-01-08 14:13:51 +0100

    Experiment with CSP headers, as the wagtail admin sidebar broke in wagtail>=6.3

--- wagtail_api_forms/settings/base.py
+++ wagtail_api_forms/settings/base.py
@@ -16,7 +16,7 @@ PROJECT_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = PROJECT_DIR.parent
 DATA_DIR = BASE_DIR / "_data"
 DB_DIR = DATA_DIR / "db"
-ASSETS_DIR = BASE_DIR / '_run' / 'assets'
+ASSETS_DIR = BASE_DIR / "_run" / "assets"
 
 
 # https://django-environ.readthedocs.io/en/latest/quickstart.html
@@ -25,7 +25,7 @@ env = environ.Env(
     DEBUG=(bool, False),
     AUTH_LDAP=(bool, False),
 )
-environ.Env.read_env(BASE_DIR / '.env')
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Make sure directory structure exists
 Path(DB_DIR).mkdir(parents=True, exist_ok=True)
@@ -37,118 +37,111 @@ DEBUG = env.bool("DEBUG")
 # Application definition
 INSTALLED_APPS = [
     # Local apps
-    'wagtail_api_forms.users',
-    'wagtail_api_forms.home',
-    'wagtail_api_forms.formpages',
-
+    "wagtail_api_forms.users",
+    "wagtail_api_forms.home",
+    "wagtail_api_forms.formpages",
     # Wagtail apps
-    'wagtail.contrib.settings',
-    'wagtail.contrib.forms',
-    'wagtail.contrib.redirects',
-    'wagtail.contrib.routable_page',
-    'wagtail.embeds',
-    'wagtail.sites',
-    'wagtail.users',
-    'wagtail.snippets',
-    'wagtail.documents',
-    'wagtail.images',
-    'wagtail.search',
-    'wagtail.admin',
-    'wagtail',
-
+    "wagtail.contrib.settings",
+    "wagtail.contrib.forms",
+    "wagtail.contrib.redirects",
+    "wagtail.contrib.routable_page",
+    "wagtail.embeds",
+    "wagtail.sites",
+    "wagtail.users",
+    "wagtail.snippets",
+    "wagtail.documents",
+    "wagtail.images",
+    "wagtail.search",
+    "wagtail.admin",
+    "wagtail",
     # https://docs.wagtail.io/en/latest/reference/contrib/simple_translation.html
-    'wagtail.locales',
-    'wagtail.contrib.simple_translation',
-    
+    "wagtail.locales",
+    "wagtail.contrib.simple_translation",
     # https://github.com/wagtail/wagtail-localize
     # 'wagtail_localize',
     # 'wagtail_localize.locales',  # This replaces 'wagtail.locales'
-
     # Third party apps
-    'wagtail_modeladmin',
-    'whitenoise.runserver_nostatic',
-    'private_storage',
+    "wagtail_modeladmin",
+    "whitenoise.runserver_nostatic",
+    "private_storage",
     # 'cspreports',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'modelcluster',
-    'taggit',
-    'crispy_forms',
-    'crispy_bootstrap5',
-    'captcha',
-    'huey.contrib.djhuey',
-
+    "rest_framework",
+    "rest_framework.authtoken",
+    "modelcluster",
+    "taggit",
+    "crispy_forms",
+    "crispy_bootstrap5",
+    "captcha",
+    "huey.contrib.djhuey",
     # Django apps
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'csp.middleware.CSPMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "csp.middleware.CSPMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     # Using our customized WhiteNoiseMiddleware to serve additional
     # static files (here: /docs/)
     # 'whitenoise.middleware.WhiteNoiseMiddleware',
-    'wagtail_api_forms.home.middleware.MoreWhiteNoiseMiddleware',
-
-    'django.middleware.locale.LocaleMiddleware',
-    'wagtail.contrib.redirects.middleware.RedirectMiddleware',
+    "wagtail_api_forms.home.middleware.MoreWhiteNoiseMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
-ROOT_URLCONF = 'wagtail_api_forms.urls'
+ROOT_URLCONF = "wagtail_api_forms.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            PROJECT_DIR / 'templates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            PROJECT_DIR / "templates",
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.i18n',
-                'wagtail.contrib.settings.context_processors.settings',
-                'wagtail_api_forms.home.context_processors.branding',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
+                "wagtail.contrib.settings.context_processors.settings",
+                "wagtail_api_forms.home.context_processors.branding",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'wagtail_api_forms.wsgi.application'
+WSGI_APPLICATION = "wagtail_api_forms.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DB_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DB_DIR / "db.sqlite3",
     }
 }
 
-DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 # Cache
 # https://docs.djangoproject.com/en/4.0/topics/cache/#database-caching
 CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
     },
 }
 
@@ -157,13 +150,21 @@ CACHES = {
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = "users.CustomUser"
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
@@ -173,8 +174,8 @@ AUTHENTICATION_BACKENDS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Europe/Berlin'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "Europe/Berlin"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
@@ -182,12 +183,12 @@ FIRST_DAY_OF_WEEK = 1
 WAGTAIL_I18N_ENABLED = True
 
 WAGTAIL_CONTENT_LANGUAGES = LANGUAGES = [
-    ('en', _('English')),
-    ('de', _('German')),
+    ("en", _("English")),
+    ("de", _("German")),
 ]
 
 LOCALE_PATHS = [
-    'locale',
+    "locale",
 ]
 
 
@@ -195,13 +196,13 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
 STATICFILES_DIRS = [
     ASSETS_DIR,
-    PROJECT_DIR / 'static',
+    PROJECT_DIR / "static",
 ]
 
 # ManifestStaticFilesStorage is recommended in production, to prevent outdated
@@ -212,11 +213,11 @@ STATICFILES_DIRS = [
 # https://whitenoise.readthedocs.io/en/latest/django.html#add-compression-and-caching-support
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATIC_ROOT = BASE_DIR / '_run' / 'staticfiles'
-STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "_run" / "staticfiles"
+STATIC_URL = "/static/"
 
-MEDIA_ROOT = DATA_DIR / 'media'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = DATA_DIR / "media"
+MEDIA_URL = "/media/"
 
 
 SITE_ID = 1
@@ -238,7 +239,7 @@ SITE_ID = 1
 
 # Email these people full exception information
 # https://docs.djangoproject.com/en/1.9/ref/settings/#admins
-ADMINS = [x.split(':') for x in env.list('DJANGO_ADMINS')]
+ADMINS = [x.split(":") for x in env.list("DJANGO_ADMINS")]
 MANAGERS = ADMINS
 DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
 SERVER_EMAIL = env.str("SERVER_EMAIL")
@@ -250,10 +251,10 @@ EMAIL_SUBJECT_PREFIX = env.str("EMAIL_SUBJECT_PREFIX")
 WAGTAIL_SITE_NAME = "wagtail_api_forms"
 WAGTAIL_ALLOW_UNICODE_SLUGS = False
 WAGTAIL_MODERATION_ENABLED = False
-WAGTAILDOCS_EXTENSIONS = ['pdf', 'txt']  # 'docx'
+WAGTAILDOCS_EXTENSIONS = ["pdf", "txt"]  # 'docx'
 WAGTAILIMAGES_MAX_UPLOAD_SIZE = 5 * 1024 * 1024  # i.e. 5MB
-WAGTAILIMAGES_IMAGE_MODEL = 'home.CustomImage'
-WAGTAILDOCS_DOCUMENT_MODEL = 'home.CustomDocument'
+WAGTAILIMAGES_IMAGE_MODEL = "home.CustomImage"
+WAGTAILDOCS_DOCUMENT_MODEL = "home.CustomDocument"
 WAGTAIL_WORKFLOW_ENABLED = False
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
@@ -289,26 +290,25 @@ CORS_URLS_REGEX = r"^/api/formsubmission/.*$"
 # https://wolfgang.reutz.at/2018/10/12/django-and-content-security-policy/
 CSP_REPORT_ONLY = False
 
-CSP_FRAME_ANCESTORS = (
-    "'self'",
-)
+CSP_FRAME_ANCESTORS = ("'self'",)
 CSP_FRAME_ANCESTORS += tuple(env.list("FORMBUILDER_CSP_FRAME_ANCESTORS"))
 
-CSP_EXCLUDE_URL_PREFIXES = (
-    "/admin",
-    "/django-admin",
-    "/docs",
-)
+# CSP_EXCLUDE_URL_PREFIXES = (
+#     "/admin",
+#     "/django-admin",
+#     "/docs",
+# )
 # unsafe-inline needed for inline CSS, e.g. via <style>...</style> or style="":
-CSP_STYLE_SRC = (
-    "'self'",
-    "'unsafe-inline'",
-)
-CSP_SCRIPT_SRC = (
-    "'self'",
-    "'unsafe-inline'",
-    "'unsafe-eval'",
-)
+# CSP_STYLE_SRC = (
+#     "'self'",
+#     "'unsafe-inline'",
+# )
+# CSP_SCRIPT_SRC = (
+#     "'self'",
+#     "'unsafe-inline'",
+#     "'unsafe-eval'",
+#     "'strict-dynamic'",
+# )
 
 
 # https://github.com/adamalton/django-csp-reports#how-do-i-use-this-thing
@@ -318,18 +318,20 @@ CSP_SCRIPT_SRC = (
 # CSP_REPORTS_SAVE = True
 # CSP_REPORTS_LOG_LEVEL = 'debug'
 
+
 # https://github.com/mbi/django-simple-captcha
 # https://starcross.dev/blog/6/customising-django-simple-captcha/
 def captcha_challenge():
     import random
 
-    challenge = ''
-    response = ''
+    challenge = ""
+    response = ""
     for i in range(4):
-        digit = random.randint(0,9)
+        digit = random.randint(0, 9)
         challenge += str(digit)
         response += str((digit + 1) % 10)
     return challenge, response
+
 
 CAPTCHA_FONT_SIZE = 34
 CAPTCHA_BACKGROUND_COLOR = "#006c66"
@@ -340,7 +342,7 @@ CAPTCHA_CHALLENGE_FUNCT = captcha_challenge
 
 
 # https://github.com/edoburu/django-private-storage
-PRIVATE_STORAGE_ROOT = DATA_DIR / 'attachments/'
+PRIVATE_STORAGE_ROOT = DATA_DIR / "attachments/"
 # PRIVATE_STORAGE_AUTH_FUNCTION = 'private_storage.permissions.allow_authenticated'
 
 SESSION_ENGINE = "django.contrib.sessions.backends.db"
@@ -351,14 +353,14 @@ _HUEY_FILENAME = "wagtailapiforms_huey"
 _HUEY_DB_FILEPATH = f"{DATA_DIR}/db/{_HUEY_FILENAME}.sqlite"
 
 HUEY = {
-    'huey_class': 'huey.SqliteHuey',
-    'filename': _HUEY_DB_FILEPATH,
-    'name': _HUEY_FILENAME,
-    'immediate': False,
-    'results': True,
-    'store_none': True,
-    'consumer': {
-        'workers': 2,  # use 2 threads
+    "huey_class": "huey.SqliteHuey",
+    "filename": _HUEY_DB_FILEPATH,
+    "name": _HUEY_FILENAME,
+    "immediate": False,
+    "results": True,
+    "store_none": True,
+    "consumer": {
+        "workers": 2,  # use 2 threads
     },
 }
 
@@ -381,13 +383,23 @@ FORMBUILDER_DEFAULT_CSS_VARIABLES = {
     "primary_accent_gray": "rgb(216, 216, 216)",
 }
 
-FORMBUILDER_WHITELIST_IPS_ATTACHMENT_REQUEST = env.list("FORMBUILDER_WHITELIST_IPS_ATTACHMENT_REQUEST")
+FORMBUILDER_WHITELIST_IPS_ATTACHMENT_REQUEST = env.list(
+    "FORMBUILDER_WHITELIST_IPS_ATTACHMENT_REQUEST"
+)
 
 # Local settings populated by .env
-FORMBUILDER_USE_ANTIVIR_SERVICE = env.bool("FORMBUILDER_USE_ANTIVIR_SERVICE", default=True)
-FORMBUILDER_MAX_UPLOAD_SIZE = env.int("FORMBUILDER_MAX_UPLOAD_MB", default=3) * 1000 * 1000
-FORMBUILDER_ALLOWED_DOCUMENT_FILE_TYPES = env.list("FORMBUILDER_ALLOWED_DOCUMENT_FILE_TYPES", default=[".pdf", ".txt"])
-FORMBUILDER_ALLOWED_IMAGE_FILE_TYPES = env.list("FORMBUILDER_ALLOWED_IMAGE_FILE_TYPES", default=[".jpg", ".jpeg", ".png"])
+FORMBUILDER_USE_ANTIVIR_SERVICE = env.bool(
+    "FORMBUILDER_USE_ANTIVIR_SERVICE", default=True
+)
+FORMBUILDER_MAX_UPLOAD_SIZE = (
+    env.int("FORMBUILDER_MAX_UPLOAD_MB", default=3) * 1000 * 1000
+)
+FORMBUILDER_ALLOWED_DOCUMENT_FILE_TYPES = env.list(
+    "FORMBUILDER_ALLOWED_DOCUMENT_FILE_TYPES", default=[".pdf", ".txt"]
+)
+FORMBUILDER_ALLOWED_IMAGE_FILE_TYPES = env.list(
+    "FORMBUILDER_ALLOWED_IMAGE_FILE_TYPES", default=[".jpg", ".jpeg", ".png"]
+)
 
 
 # Setting variations between development and production (see end of this settings file)
@@ -408,7 +420,9 @@ AUTH_LDAP = env.bool("AUTH_LDAP")
 
 if DEBUG:
     print(f"{AUTH_LDAP=}")
-    print(f"FORMBUILDER_MAX_UPLOAD_SIZE in MegaBytes is {FORMBUILDER_MAX_UPLOAD_SIZE / (1024 * 1024)}")
+    print(
+        f"FORMBUILDER_MAX_UPLOAD_SIZE in MegaBytes is {FORMBUILDER_MAX_UPLOAD_SIZE / (1024 * 1024)}"
+    )
     print(f"{FORMBUILDER_ALLOWED_DOCUMENT_FILE_TYPES=}")
     print(f"{FORMBUILDER_ALLOWED_IMAGE_FILE_TYPES=}")
     print(f"{FORMBUILDER_USE_ANTIVIR_SERVICE=}")
@@ -420,5 +434,5 @@ if DEBUG:
     SESSION_COOKIE_SECURE = False
     CSRF_COOKIE_SECURE = False
     ALLOWED_HOSTS = ["*"]
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     # SENDFILE_BACKEND = 'sendfile.backends.development'
