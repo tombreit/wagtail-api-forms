2a6e4f97131ed3f36754104a4a378529085e692b
Thomas Breitner <mail@thms.de>
2025-05-26 13:03:39 +0200

    Formatting

--- wagtail_api_forms/settings/ldap.py
+++ wagtail_api_forms/settings/ldap.py
@@ -15,8 +15,8 @@ from .base import (
 
 AUTH_LDAP = env.bool("AUTH_LDAP")
 AUTH_LDAP_SERVER_URI = env.str("AUTH_LDAP_SERVER_URI")
-AUTH_LDAP_BIND_DN = env.str('AUTH_LDAP_BIND_DN')
-AUTH_LDAP_BIND_PASSWORD = env.str('AUTH_LDAP_BIND_PASSWORD')
+AUTH_LDAP_BIND_DN = env.str("AUTH_LDAP_BIND_DN")
+AUTH_LDAP_BIND_PASSWORD = env.str("AUTH_LDAP_BIND_PASSWORD")
 AUTH_LDAP_MIRROR_GROUPS = env.list("AUTH_LDAP_MIRROR_GROUPS")
 
 if env.bool("DEBUG"):
@@ -25,7 +25,9 @@ if env.bool("DEBUG"):
     print(f"{AUTH_LDAP=}: against {AUTH_LDAP_SERVER_URI}")
 
 # First check LDAPBackend, than ModelBackend
-AUTHENTICATION_BACKENDS = ['django_auth_ldap.backend.LDAPBackend'] + AUTHENTICATION_BACKENDS
+AUTHENTICATION_BACKENDS = [
+    "django_auth_ldap.backend.LDAPBackend"
+] + AUTHENTICATION_BACKENDS
 AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
 AUTH_LDAP_FIND_GROUP_PERMS = True
 
@@ -39,35 +41,31 @@ AUTH_LDAP_USER_SEARCH = LDAPSearchUnion(
 )
 
 AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
-    f"{env.str('AUTH_LDAP_USERS_DN')}",
-    ldap.SCOPE_SUBTREE,
-    "(objectClass=group)"
+    f"{env.str('AUTH_LDAP_USERS_DN')}", ldap.SCOPE_SUBTREE, "(objectClass=group)"
 )
 
 AUTH_LDAP_USER_ATTR_MAP = {
     "first_name": "givenName",
     "last_name": "sn",
-    "email": "mail"
+    "email": "mail",
 }
 
 # https://django-auth-ldap.readthedocs.io/en/latest/groups.html#limiting-access
 AUTH_LDAP_REQUIRE_GROUP = (
-    LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS")) | 
-    LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF")) |
-    LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_EDITORS"))
+    LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS"))
+    | LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF"))
+    | LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_EDITORS"))
 )
 
 AUTH_LDAP_USER_FLAGS_BY_GROUP = {
     "is_active": (
-        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS")) | 
-        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF")) |
-        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_EDITORS"))
+        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS"))
+        | LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF"))
+        | LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_EDITORS"))
     ),
     "is_staff": (
-        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF")) |
-        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_EDITORS"))
+        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_STAFF"))
+        | LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_EDITORS"))
     ),
-    "is_superuser": (
-        LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS"))
-    ),
+    "is_superuser": (LDAPGroupQuery(env.str("AUTH_LDAP_GROUP_SUPERUSERS"))),
 }
